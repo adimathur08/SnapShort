5e698a3476f918599ff9fbec366b242003220180
Aditya Mathur <adimathur08@gmail.com>
2024-04-24 01:01:03 +0530

    User Management - removed repository

--- user-management/src/main/java/com/amathur/snapshort/usermanagement/service/UserDeleteService.java
+++ user-management/src/main/java/com/amathur/snapshort/usermanagement/service/UserDeleteService.java
@@ -1,12 +1,8 @@
 package com.amathur.snapshort.usermanagement.service;
 
-import com.amathur.snapshort.usermanagement.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserDeleteService
 {
-    @Autowired
-    UserRepository repository;
 }
