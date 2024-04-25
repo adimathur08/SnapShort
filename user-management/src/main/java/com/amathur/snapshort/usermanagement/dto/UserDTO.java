08e6c3d20cb4e33ae3b562ae13c3fb03345a9792
Aditya Mathur <adimathur08@gmail.com>
2024-04-26 02:41:06 +0530

    User Management - port change and added user constraints

--- user-management/src/main/java/com/amathur/snapshort/usermanagement/dto/UserDTO.java
+++ user-management/src/main/java/com/amathur/snapshort/usermanagement/dto/UserDTO.java
@@ -9,13 +9,18 @@ public class UserDTO
 {
     @Valid
 
-    @NotNull(message = "Name of the user cannot e null")
-    @NotBlank(message = "Name of the user cannot e blank")
+    @NotNull(message = "Name of the user cannot be null")
+    @NotBlank(message = "Name of the user cannot be blank")
     @Size(min = 3, max = 10, message = "Name of the user should be in range 3-10")
     String name;
 
-    @NotNull(message = "Username cannot e null")
-    @NotBlank(message = "Username cannot e blank")
+    @NotNull(message = "Username cannot be null")
+    @NotBlank(message = "Username cannot be blank")
     @Size(min = 3, max = 10, message = "Username should be in range 3-10")
     String username;
+
+    @NotNull(message = "Password cannot be null")
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 5, max = 15, message = "Password should be in range 5-15")
+    String password;
 }
