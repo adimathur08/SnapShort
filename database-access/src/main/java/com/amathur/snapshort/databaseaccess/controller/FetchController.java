7f3dff6bfc5897738d543f16e0164532ff2e06a4
Aditya Mathur <adimathur08@gmail.com>
2024-04-28 18:23:26 +0530

    Database Access - API Path change

--- database-access/src/main/java/com/amathur/snapshort/databaseaccess/controller/FetchController.java
+++ database-access/src/main/java/com/amathur/snapshort/databaseaccess/controller/FetchController.java
@@ -13,7 +13,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/user/fetch")
+@RequestMapping("/internal/user/fetch")
 public class FetchController
 {
     @Autowired
--- database-access/src/main/java/com/amathur/snapshort/databaseaccess/controller/RegisterController.java
+++ database-access/src/main/java/com/amathur/snapshort/databaseaccess/controller/RegisterController.java
@@ -22,7 +22,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/internal/user")
 public class RegisterController
 {
     @Autowired
