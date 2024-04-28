50a818979e0a477d603e23769a2c931b2e47a1c1
Aditya Mathur <adimathur08@gmail.com>
2024-04-28 23:13:03 +0530

    Post Content - init :)

---
+++ post-content/src/main/java/com/amathur/snapshort/postcontent/PostContentApplication.java
@@ -0,0 +1,13 @@
+package com.amathur.snapshort.postcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostContentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostContentApplication.class, args);
+	}
+
+}
---
+++ post-content/src/test/java/com/amathur/snapshort/postcontent/PostContentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amathur.snapshort.postcontent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostContentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
